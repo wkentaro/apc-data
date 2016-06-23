5751d175a21af320acba72850c43bfb657d018e2
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-06-23 13:45:43 +0900

    Enhance download.py for APC2016jsk

--- APC2016jsk/download.py
+++ APC2016jsk/download.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -5,10 +7,50 @@ from __future__ import print_function
 import os.path as osp
 import sys
 
-this_dir = osp.dirname(osp.realpath(__file__))
-sys.path.insert(0, osp.join(this_dir, '..'))
 from util import download_data
 
+
+download_data(
+    url='https://drive.google.com/uc?id=0B9P1L--7Wd2vOEwzNW1vN3JYRWM',
+    md5='3bf22986e2f7bf7e50f6c9e10bd0d6c3',
+    path='2016-06-16-07-48-44.tgz',
+    extract=True
+)
+
+download_data(
+    url='https://drive.google.com/uc?id=0B9P1L--7Wd2vcHJlaHRVWXZqcnM',
+    md5='e20398f98b20128e3387276f06dce043',
+    path='2016-06-16-23-05-00.tgz',
+    extract=True
+)
+
+download_data(
+    url='https://drive.google.com/uc?id=0B9P1L--7Wd2vMjU0NDhGYVNOYjA',
+    md5='001b685020a002c5ff931fae171fcc22',
+    path='2016-06-17-18-07-48.tgz',
+    extract=True
+)
+
+download_data(
+    url='https://drive.google.com/uc?id=0B9P1L--7Wd2vZUNTV3FUV3ZkWWs',
+    md5='a31fb6fa1927944fe9edfff4392243ac',
+    path='2016-06-19-12-51-19.tgz',
+    extract=True
+)
+
+download_data(
+    url='https://drive.google.com/uc?id=0B9P1L--7Wd2vbEo2OE4tYklKTlU',
+    md5='e1c647bdf9b6c107192cf3f43c3ddf69',
+    path='2016-06-19-19-31-45.tgz',
+    extract=True
+)
+
+download_data(
+    url='https://drive.google.com/uc?id=0B9P1L--7Wd2vcHA5TUlYNXd0WkE',
+    md5='7fb5eb7356b2a0dac744d168fb37ff93',
+    path='2016-06-19-20-30-18.tgz',
+    extract=True
+)
 
 download_data(
     url='https://drive.google.com/uc?id=0B9P1L--7Wd2vOU5WSWxvRzBGNEE',
