e73b7aee05f9562025933d672a2f8b47453eca73
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-06-23 14:35:56 +0900

    Update download.py

--- APC2016jsk/download.py
+++ APC2016jsk/download.py
@@ -11,50 +11,8 @@ from util import download_data
 
 
 download_data(
-    url='https://drive.google.com/uc?id=0B9P1L--7Wd2vcDhBWEF4WXhPR2s',
-    md5='3cd7c211691fd4d9a3cf88ecdca99e25',
-    path='2016-06-16-07-48-44.tgz',
+    url='https://drive.google.com/uc?id=0B9P1L--7Wd2vMVNlR1JNV1RXLVE',
+    md5='2da610302072f99ba7aa34ab145c4b0d',
+    path='all.tgz',
     extract=True
-)
-
-download_data(
-    url='https://drive.google.com/uc?id=0B9P1L--7Wd2vV2FRMjhQUEQ2S1E',
-    md5='88f6dc76b105889bb0e859c12b56ef4c',
-    path='2016-06-16-23-05-00.tgz',
-    extract=True
-)
-
-download_data(
-    url='https://drive.google.com/uc?id=0B9P1L--7Wd2vclYyTWFpLXNTdWc',
-    md5='aa492f4ac15a721124032753b1463e38',
-    path='2016-06-17-18-07-48.tgz',
-    extract=True
-)
-
-download_data(
-    url='https://drive.google.com/uc?id=0B9P1L--7Wd2vLXlYUU5waTQ3blk',
-    md5='876b0aae266ef8adbb038e85dc56a628',
-    path='2016-06-19-12-51-19.tgz',
-    extract=True
-)
-
-download_data(
-    url='https://drive.google.com/uc?id=0B9P1L--7Wd2vU2EyeHNDdE1LNWs',
-    md5='ca3caaaeac2d2525cd9614196a272284',
-    path='2016-06-19-19-31-45.tgz',
-    extract=True
-)
-
-download_data(
-    url='https://drive.google.com/uc?id=0B9P1L--7Wd2va2dsRmVrUzh6a00',
-    md5='0dd51ac7b7f90bab502a81a0cc4c3313',
-    path='2016-06-19-20-30-18.tgz',
-    extract=True
-)
-
-download_data(
-    url='https://drive.google.com/uc?id=0B9P1L--7Wd2vMHdxWVdWSXEyWnM',
-    md5='f6147d52f62d2072161f0182de56c4ca',
-    path='2016-06-22-01-06-02.tgz',
-    extract=True,
 )
